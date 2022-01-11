8b757bf63372202e3de7fb47aa7588a0e22d9bb1
BearingMe <gomes.bruno201@gmail.com>
2022-01-11 13:23:07 -0300

    feat: inicia os models

---
+++ src/models/productsModel.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "product name must be provided"],
+    maxlength: 100,
+  },
+
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+    maxlength: 20,
+  },
+
+  company: {
+    type: String,
+    required: [true, "product company must be provided"],
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "invalid company",
+    },
+  },
+
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0,
+    maxlength: 1,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("products", productsSchema);
