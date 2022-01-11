98a8f666b787c6f6aae32f4efad72e47f7221dff
BearingMe <gomes.bruno201@gmail.com>
2022-01-11 11:05:10 -0300

    feat: inicia o express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+
+// constants
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// start the server
+app.listen(port, () => console.log(`Server started at port ${port}`));
