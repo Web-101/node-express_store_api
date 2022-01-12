29b0c32c86d7793142029f15d434f4e5763cb5d9
BearingMe <gomes.bruno201@gmail.com>
2022-01-12 16:34:15 -0300

    feat: adiciona funcionalidade aos controllers

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import productsModel from "../models/productsModel";
+import * as controller from "./productsProtocols";
 
 // no params required
 export function getAllProducts(req: express.Request, res: express.Response) {
-  res.status(200).json({ message: "get all products" });
+  const query: controller.queryProtocol = {};
+  req.query.name && (query.name = String(req.query.name));
+  req.query.company && (query.company = String(req.query.company));
+  req.query.featured && (query.featured = Boolean(req.query.featured));
+  req.query.rating && (query.rating = Number(req.query.rating));
+  req.query.price && (query.price = Number(req.query.price));
+  
+  console.log(query);
+
+  productsModel
+    .find(query)
+    .then((products) => res.json(products.length))
+    .catch((err) => res.status(500).json(err));
 }
 
 export function createProduct(req: express.Request, res: express.Response) {
---
+++ src/controllers/productsProtocols.ts
@@ -0,0 +1,7 @@
+export interface queryProtocol {
+  name?: string;
+  company?: string;
+  featured?: boolean;
+  rating?: number;
+  price?: number;
+}
